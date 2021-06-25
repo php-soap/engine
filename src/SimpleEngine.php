03044f41a699d0b9f99072ed9ed6e509040ecc9f
Toon Verwerft <toonverwerft@gmail.com>
2021-06-25 06:51:23 +0200

    Add engine

---
+++ src/SimpleEngine.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Soap\Engine;
+
+use Soap\Engine\Metadata\Metadata;
+
+final class SimpleEngine implements Engine
+{
+    private Driver $driver;
+    private Transport $transport;
+
+    public function __construct(
+        Driver $driver,
+        Transport $transport
+    ) {
+        $this->driver = $driver;
+        $this->transport = $transport;
+    }
+
+    public function request(string $method, array $arguments)
+    {
+        $request = $this->driver->encode($method, $arguments);
+        $response = $this->transport->request($request);
+
+        return $this->driver->decode($method, $response);
+    }
+
+    public function getMetadata(): Metadata
+    {
+        $this->driver->getMetadata();
+    }
+}
